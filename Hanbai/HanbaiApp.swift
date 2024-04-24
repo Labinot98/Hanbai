400a4f9786dcbef8a618a2b51b1f8f7ff04437ed
Labinot Pajaziti <38728474+Labinot98@users.noreply.github.com>
2024-04-24 15:43:16 +0200

    Initial Commit

---
+++ Hanbai/HanbaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanbaiApp.swift
+//  Hanbai
+//
+//  Created by Pajaziti Labinot on 24.4.24..
+//
+
+import SwiftUI
+
+@main
+struct HanbaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
